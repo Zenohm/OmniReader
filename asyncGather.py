13a61033f3634a601c2dd2937a965f71996b3ae4
Zenohm <sentherus@gmail.com>
2015-09-19 12:27:38 -0400

    Possible async story gatherer
    Code may be useful for future versions.

---
+++ asyncGather.py
@@ -0,0 +1,15 @@
+import asyncio
+
+full_text = []
+
+
+async def fetch(url):
+    print("Task %s: Fetching..." % (url.text))
+    url.initialize()
+    await asyncio.sleep(1)
+    full_text.append(url.text)
+    print("%s is fucking DONE bruh." % (url.url))
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(fetch(url)) for url in full_frontal_storytime]
+loop.run_until_complete(asyncio.wait(tasks))
