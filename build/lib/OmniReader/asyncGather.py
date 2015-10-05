6f2616798fbb34019c3773e0d4bed353c6a1dc2b
Zenohm <sentherus@gmail.com>
2015-10-04 23:17:07 -0400

    Install Update
    This library can now be installed!
    Full information added

---
+++ build/lib/OmniReader/asyncGather.py
@@ -0,0 +1,33 @@
+import asyncio
+
+def afetch(stories):
+    full_text = []
+    async def fetch(url):
+        print("Task %s: Fetching..." % (url.text))
+        url.initialize()
+        await asyncio.sleep(.1)
+        full_text.append(url.text)
+        print("%s Complete." % (url.url))
+    
+    loop = asyncio.get_event_loop()
+    tasks = [asyncio.ensure_future(fetch(url)) for url in stories]
+    loop.run_until_complete(asyncio.wait(tasks))
+    return full_text
+
+"""
+import asyncio
+
+full_text = []
+
+
+async def fetch(url):
+    print("Task %s: Fetching..." % (url.text))
+    url.initialize()
+    await asyncio.sleep(1)
+    full_text.append(url.text)
+    print("%s completed" % (url.url))
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(fetch(url)) for url in full_frontal_storytime]
+loop.run_until_complete(asyncio.wait(tasks))
+"""
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+
+version_file = os.path.join(os.path.dirname(__file__),'OmniReader', 'VERSION')
+with open(version_file) as fh:
+    omnireader_version = fh.read().strip()
+
+
+from distutils.core import setup
+
+setup(
+    name = "OmniReader",
+    description = "OmniReader Story Teller",
+    version = omnireader_version,
+    url = "https://github.com/Zenohm/OmniReader",
+    long_description = """\
+The OmniReader Story Teller is a python package that reads stories
+using a TTS engine.  OmniReader requires Python 3.3+.""",
+    license = "MIT",
+    keywords = ['OmniReader', 'Fanfiction', 'stories',
+                'scraping', 'translating', 'Wattpad',
+                'text-to-speech', 'deviantart'],
+    maintainer = "Isaac Smith (Zenohm)",
+    maintainer_email = "sentherus@gmail.com",
+    author = "Isaac Smith (Zenohm)",
+    author_email = "sentherus@gmail.com",
+    classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Other Audience',
+    'Intended Audience :: Education',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Multimedia :: Sound/Audio',
+    'Topic :: Multimedia :: Sound/Audio :: Speech',
+    ],
+    packages = ['OmniReader'],
+    )
