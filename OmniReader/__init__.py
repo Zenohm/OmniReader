61494b1eda833ac4a55cbe0bcd28603f44050e17
Isaac Smith <sentherus@gmail.com>
2015-10-05 13:41:09 -0400

    Fetch version
    The library now determines its version based on the `VERSION` file.

--- OmniReader/__init__.py
+++ OmniReader/__init__.py
@@ -3,7 +3,9 @@ __email__ = 'sentherus@gmail.com'
 __copyright__ = '2015 sentherus@gmail.com'
 __license__ = "MIT"
 __date__ = '2015-10-04'
-__version_info__ = (2, 1, 1)
+version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+with open(version_file) as fh:
+    __version_info__ = fh.read().strip()
 __version__ = '.'.join(str(i) for i in __version_info__)
 __home__ = 'https://github.com/Zenohm/OmniReader'
 __download__ = 'https://github.com/Zenohm/OmniReader/archive/master.zip'
