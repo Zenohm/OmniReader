f24495b0dfd09a4a7d61ac336e3eb429c446bcac
Isaac <sentherus@gmail.com>
2017-01-09 13:51:37 -0500

    Update utility_functions.py
    Simplify say function while adding more cross-platform support.

--- OmniReader/utility_functions.py
+++ OmniReader/utility_functions.py
@@ -1,11 +1,6 @@
 import os
-import sys
-import webbrowser
 from gtts import gTTS
-from importlib import import_module
-from platform import python_version
 from subprocess import call
-from textwrap import fill
 home = os.path.expanduser("~")
 
 speech_system = 'google'
@@ -16,27 +11,23 @@ def continue_question(intro_text='reached the end',
                       end_text='start another story'):
     answer = input("You have " + intro_text +
                    ", do you want to " + end_text + "? ")
-    if answer == '1' or answer.startswith(('Y', 'y')):
-        return True
-    else:
-        return False
+    return answer == '1' or answer.startswith(('Y', 'y'))
 
 
 def say(message, title='Speak', speech_system='google', say=True, lang='en'):
     if speech_system == 'google':
         # Create the MP3 file which will speak the text
         folder = ''
-        if '\\' in title:
-            folder = '\\'.join(title.split('\\')[:-1])
+        folder, file_name = os.path.split(title)
         title += '.mp3'
         tts = gTTS(message, lang=lang)
-        path = home+'\\'+title
-        folder_path = home + "\\" + folder
+        path = os.path.join(home, title)
+        folder_path = os.path.join(home, folder)
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
         tts.save(path)
         if say:
-            call("start /MIN {}".format(home+'\\'+title), shell=True)
+            call("start /MIN {}".format(os.path.join(home, title)), shell=True)
     else:
         # Create the Visual Basic code which will speak the text
         with open(title + '.vbs', 'w') as file:
@@ -47,8 +38,7 @@ def say(message, title='Speak', speech_system='google', say=True, lang='en'):
                     Set speech=CreateObject("sapi.spvoice")
                     speech.Speak speaks
                     """
-                    .format(
-                        str(message).replace('"', '').replace('\n', '')))
+                    .format(str(message).replace('"', '').replace('\n', '')))
         # Execute the file
         call(['cscript.exe', title + '.vbs'])
 
